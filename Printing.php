4b8c9dd0c4e7d9cecd38482685137162cd697ab0
azhari <azharisaputrasikumbang@gmail.com>
2021-05-01 16:28:06 +0700

    Week 1 : Printing and Dumping

---
+++ Printing.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Text yang akan dicetak";
+echo "<br>";
+
+print "Text lain yang akan dicetak";
+echo "<br>";
+
+printf("Text dicetak tetapi dengan format %d", 100);
+echo "<br>";
+
+print_r("Mencetak data beserta informasi yang lebih mudah dibaca");
+echo "<br>";
+
+var_dump("Mencetak data beserta informasin lengkapnya");
